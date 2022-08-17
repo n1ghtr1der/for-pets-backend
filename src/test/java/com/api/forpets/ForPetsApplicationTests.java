000e3e36fe0d2d39d33e9e966304df5facc47566
Emirton Sousa <emirtonf@gmail.com>
2022-08-17 10:44:26 -0300

    feat(first commit): project generated at https://start.spring.io.

---
+++ src/test/java/com/api/forpets/ForPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.forpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
