5a1de095baa0c56aee39ade7f56ff1f4db745877
Emirton Sousa <emirtonf@gmail.com>
2022-08-18 18:18:52 -0300

    feat: services methods declared on repository class.

--- src/main/java/com/api/forpets/repositories/AnimalRepository.java
+++ src/main/java/com/api/forpets/repositories/AnimalRepository.java
@@ -1,7 +1,16 @@
 package com.api.forpets.repositories;
 
 import com.api.forpets.models.AnimalModel;
+import com.api.forpets.models.AnimalSpecieEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+import java.util.List;
+
+@Repository
 public interface AnimalRepository extends JpaRepository<AnimalModel, Long> {
+    void deleteByAnimalId(Long id);
+    List<AnimalModel> findBySpecie(AnimalSpecieEnum specie);
+    List<AnimalModel> findByWeight(double weight);
 }
