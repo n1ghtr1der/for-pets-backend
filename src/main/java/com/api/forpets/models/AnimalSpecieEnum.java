b76a531888b80d024316fe39a0fc891e59cf1d28
Emirton Sousa <emirtonf@gmail.com>
2022-08-17 22:06:19 -0300

    feat: models classes created.

---
+++ src/main/java/com/api/forpets/models/AnimalSpecieEnum.java
@@ -0,0 +1,18 @@
+package com.api.forpets.models;
+
+public enum AnimalSpecieEnum {
+    BIRD("Bird"),
+    FISH("Fish"),
+    REPTILE("Reptile"),
+    MAMMAL("Mammal");
+
+    private final String displayName;
+
+    AnimalSpecieEnum(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+}
